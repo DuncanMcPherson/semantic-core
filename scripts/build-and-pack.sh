8de83fbf96dfa4d5e3f169966b4fed6b92b0b3a6
Duncan McPherson <d.mcpherson.home@gmail.com>
2025-07-09 14:09:30 -0600

    fix: use variable instead of undefined value

--- scripts/build-and-pack.sh
+++ scripts/build-and-pack.sh
@@ -4,4 +4,4 @@ RELEASE_NOTES=$2
 
 dotnet restore
 dotnet build -c Release --no-restore
-dotnet pack -c Release --no-build -p:Version=VERSION -p:PackageReleaseNotes="$RELEASE_NOTES" -o ./artifacts
+dotnet pack -c Release --no-build -p:Version="$VERSION" -p:PackageReleaseNotes="$RELEASE_NOTES" -o ./artifacts
