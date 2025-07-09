0256b7b80288393aea0142c0c8cb6002a1c294e2
Duncan McPherson <d.mcpherson.home@gmail.com>
2025-07-09 11:29:52 -0600

    Initial commit

---
+++ CorePlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SemanticRelease.Abstractions;
+
+namespace SemanticRelease.Core
+{
+    public class CorePlugin : ISemanticPlugin
+    {
+        public void Register(SemanticLifecycle lifecycle)
+        {
+            lifecycle.OnAnalyzeCommits(async context =>
+            {
+                
+            });
+        }
+    }
+}
